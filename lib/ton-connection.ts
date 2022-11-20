6f3f30c580a7c878b138976e953b46436bd9dc32
Shahar Yakir <shahar.yakir@gmail.com>
2022-11-20 14:07:54 +0200

    removed get parser

--- lib/ton-connection.ts
+++ lib/ton-connection.ts
@@ -1,4 +1,4 @@
-import { Address, Cell, StateInit, TonClient } from "ton";
+import { Address, Cell, StateInit } from "ton";
 import BN from "bn.js";
 
 export interface Wallet {
@@ -19,15 +19,11 @@ export interface TransactionDetails {
   message?: Cell;
 }
 
-export type GetResponseValue = Cell | BN | null;
-
 export class TonConnection {
-  private _provider: TonWalletProvider | null;
-  public _tonClient: TonClient; // Future - wrap functionality and make private
+  private _provider: TonWalletProvider;
 
-  constructor(provider: TonWalletProvider | null, rpcApi: string, apiKey?: string) {
+  constructor(provider: TonWalletProvider) {
     this._provider = provider;
-    this._tonClient = new TonClient({ endpoint: rpcApi, apiKey });
   }
 
   requestTransaction(request: TransactionDetails, onSuccess?: () => void): Promise<void> {
@@ -37,46 +33,5 @@ export class TonConnection {
   connect(): Promise<Wallet> {
     if (!this._provider) throw new Error("Cannot connect without a wallet provider");
     return this._provider.connect();
-  }
-
-  #parseGetMethodCall(stack: [["num" | "cell" | "list", any]]): GetResponseValue[] {
-    return stack.map(([type, val]) => {
-      switch (type) {
-        case "num":
-          return new BN(val.replace("0x", ""), "hex");
-        case "cell":
-          return Cell.fromBoc(Buffer.from(val.bytes, "base64"))[0];
-        case "list":
-          if (val.elements.length === 0) {
-            return null;
-          } else {
-            throw new Error("list parsing not supported");
-          }
-        default:
-          throw new Error(`unknown type: ${type}, val: ${JSON.stringify(val)}`);
-      }
-    });
-  }
-
-  #prepareParams(params: Cell[] = []) {
-    return params.map((p) => {
-      if (p instanceof Cell) {
-        // TODO what's idx:false
-        return ["tvm.Slice", p.toBoc({ idx: false }).toString("base64")];
-      }
-
-      throw new Error("unknown type!");
-    });
-  }
-
-  // TODO support other params than Cell
-  async makeGetCall<T>(
-    contract: Address,
-    method: string,
-    params: Cell[],
-    parser: (stack: GetResponseValue[]) => T
-  ): Promise<T> {
-    const res = await this._tonClient.callGetMethod(contract, method, this.#prepareParams(params));
-    return parser(this.#parseGetMethodCall(res.stack as [["num" | "cell", any]]));
   }
 }
--- test/ton-connection.spec.ts
+++ test/ton-connection.spec.ts
@@ -1,12 +1,11 @@
 import chai, { expect } from "chai";
 import chaiBN from "chai-bn";
 import BN from "bn.js";
-import { Address, beginCell, Cell, Wallet } from "ton";
-import { TonWalletProvider, TransactionDetails, TonConnection } from "../lib";
+import { beginCell, Cell } from "ton";
+import { TonWalletProvider, TransactionDetails, TonConnection, Wallet } from "../lib";
 import * as sinon from "ts-sinon";
 import sinonChai from "sinon-chai";
 import { randomAddress, zeroAddress } from "./utils";
-import exp from "constants";
 chai.use(chaiBN(BN));
 chai.use(sinonChai);
 
@@ -27,40 +26,8 @@ class StubProvider implements TonWalletProvider {
 
 describe("Ton Connection", () => {
   it("Retrieves wallet details from provider", async () => {
-    const con = new TonConnection(new StubProvider(), "some_api");
+    const con = new TonConnection(new StubProvider());
     const w = await con.connect();
     expect(w).to.deep.eq(walletStub);
-  });
-
-  it("Parses get methods", async () => {
-    const con = new TonConnection(new StubProvider(), "some_api");
-    const w = await con.connect();
-    expect(w).to.deep.eq(walletStub);
-    sinon.default.replace(con, "_tonClient", sinon.stubObject(con._tonClient));
-
-    con._tonClient.callGetMethod.resolves({
-      stack: [
-        [
-          "cell",
-          { bytes: beginCell().storeAddress(zeroAddress()).endCell().toBoc().toString("base64") },
-        ],
-        ["num", "0x1999"],
-      ],
-    });
-
-    const parsedResult = await con.makeGetCall(
-      randomAddress("someContract"),
-      "meth",
-      [new Cell()],
-      (d) => {
-        return {
-          address: (d[0] as Cell).beginParse().readAddress(),
-          num: d[1] as BN,
-        };
-      }
-    );
-
-    expect(parsedResult.address?.toFriendly()).to.equal(zeroAddress().toFriendly());
-    expect(parsedResult.num).to.bignumber.equal(new BN("1999", "hex"));
   });
 });
