f0cc9efdf83eb6384c36a81f3d0efdcbfa84195b
Shahar Yakir <shahar.yakir@gmail.com>
2022-11-21 23:01:46 +0200

    in prog

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,5 @@
 export * from "./ton-connection";
 export * from "./tonhub-provider";
+export * from "./tonkeeper-provider";
 export * from "./chrome-ext-provider";
 export * from "./mnemonic-provider";
-export * from "./utils";
--- lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import { Address, Cell } from "ton";
-import { GetResponseValue } from "./ton-connection";
-
-export function cellToAddress(s: GetResponseValue): Address {
-  return (s as Cell).beginParse().readAddress() as Address;
-}
