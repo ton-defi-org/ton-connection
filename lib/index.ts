c3db4fe4ae4ed2b63c335dce83d589704f377256
Shahar Yakir <shahar.yakir@gmail.com>
2022-11-29 13:26:02 +0200

    done

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,4 @@
 export * from "./ton-connection";
 export * from "./tonhub-provider";
-export * from "./tonkeeper-provider";
 export * from "./chrome-ext-provider";
 export * from "./mnemonic-provider";
