66d53a4303c1b2f18fdbb5a03f57551d93890c26
Shahar Yakir <shahar.yakir@gmail.com>
2022-12-23 16:34:34 +0200

    Tonkeeper (based on ton-connect2) (#14)
    * 0.0.5
    
    * 0.0.6
    
    * fixed for v12 serializations
    
    * 0.1.0
    
    * in prog
    
    * in prog
    
    * 0.5.0
    
    * in prog
    
    * in prog
    
    * update tonsdk
    
    * in prog
    
    * add set provider
    
    * integration works

---
+++ lib/tonkeeper-provider.ts
@@ -0,0 +1,68 @@
+import TonConnect, { IStorage } from "@tonconnect/sdk";
+import { Address } from "ton";
+import { stateInitToBuffer } from "./internal_utils";
+import { TonWalletProvider, TransactionDetails, Wallet } from "./ton-connection";
+
+export type TonkeeperProviderConfig = {
+  connectionDetails: { bridgeUrl: string; universalLink: string };
+  manifestUrl: string;
+  onSessionLinkReady: (link: string) => void;
+  storage?: IStorage;
+};
+
+export class TonkeeperProvider implements TonWalletProvider {
+  connector: TonConnect;
+  config: TonkeeperProviderConfig;
+
+  constructor(config: TonkeeperProviderConfig) {
+    this.connector = new TonConnect({ manifestUrl: config.manifestUrl, storage: config.storage });
+    this.config = config;
+  }
+
+  async connect(): Promise<Wallet> {
+    const getWalletP = new Promise<Wallet>((resolve, reject) => {
+      this.connector.onStatusChange((wallet) => {
+        try {
+          if (wallet) {
+            resolve({
+              address: Address.parse(wallet.account.address).toFriendly(),
+            });
+          } else {
+            reject("No wallet received");
+          }
+        } catch (e) {
+          reject(e);
+        }
+      }, reject);
+    });
+
+    await this.connector.restoreConnection();
+    if (!this.connector.connected) {
+      const sessionLink = this.connector.connect(this.config.connectionDetails);
+      this.config.onSessionLinkReady(sessionLink);
+    }
+
+    return getWalletP;
+  }
+
+  async requestTransaction(
+    request: TransactionDetails,
+    onSuccess?: (() => void) | undefined
+  ): Promise<void> {
+    await this.connector.sendTransaction({
+      validUntil: Date.now() + 5 * 60 * 1000,
+      messages: [
+        {
+          address: request.to.toFriendly(),
+          amount: request.value.toString(),
+          stateInit: request.stateInit
+            ? stateInitToBuffer(request.stateInit).toString("base64")
+            : undefined,
+          payload: request.message ? request.message.toBoc().toString("base64") : undefined,
+        },
+      ],
+    });
+
+    onSuccess?.();
+  }
+}
